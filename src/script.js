8163d1cde3fd5fbca2b69c5f037a92ec7baea9c6
Oleg Kavazov <kavazuch1997@gmail.com>
2025-10-23 22:22:34 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Do you have 18?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 ?", "");
+// console.log(answer + 18);
+
+// const answers = [];
+
+// // answers[0] = prompt("Як вас звати ?", "");
+// // answers[1] = prompt("Скільки вам років ?", "");
+// // answers[2] = prompt("З якого ви міста ?", "");
+
+// // document.write(answers);
+// console.log(typeof answers);
+
